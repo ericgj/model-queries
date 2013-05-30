bbe00afef887a9b0a9ed68c3aeaf26b7bfc1018d
Eric Gjertsen <ericgj72@gmail.com>
2013-05-30 15:46:47 -0400

    rewrite: split path interpolation from query string building
    add fluent interface on top of model

--- Readme.md
+++ Readme.md
@@ -9,25 +9,52 @@
 
 ## Usage
 
+### Query strings
+
 ```javascript
 
 var queries = require('model-queries');
 
 Post.use(queries);
 
-Post.query('page', 'page/:page', {limit: 'n'});
+Post.query('page', 'all');
 
-Post.page({page: 2, limit: 20}, callback);
-  //  GET /post/page/2?n=20
+Post.page().param({page: 2, limit: 20}).run( callback );
+  //=>  GET /page/all?page=2&limit=20
 
-Post.select('page/2?n=20', callback);
-  //  GET /post/page/2?n=20
+/* or parameter by parameter */
+
+Post.page().param('page',2).param('limit',20).run( callback );
+
+```
+
+### Sub-entities
+
+```javascript
+
+var queries = require('model-queries');
+
+Post.use(queries);
+
+Post.query('forUser', '/user/:id/post/all');
+  
+Post.forUser({id: 123}).run( callback );
+  //=>  GET /user/123/post/all?p=2&n=20
+
+```
+
+### Sub-entities with query strings
+
+``` javascript
+Post.forUser({id: 123}).param({p: 2, n: 20}).run( callback );
+  //=>  GET /user/123/post/all?p=2&n=20
 
 ```
 
 ## API
 
-   
+TODO
+
 
 ## License
 
--- index.js
+++ index.js
@@ -4,38 +4,56 @@ var request = require('superagent')
 
 module.exports = function anonymous(model){
   
-  model.query = function(meth,path,query){
-    this[meth] = function(params,fn){
-      var rpath = resolvePath(path,query,params);
-      return this.select(rpath,fn);
+  model.query = function(meth,path){
+    this[meth] = function(ids){
+      return new Query(this,path,ids);
     }
   }
 
-  model.select = function(path,fn){
-    var self = this
-      , url  = this.url(path);
+  function Query(model,path,ids){
+    this.model = model; this.path = path; this.ids = ids;
+    this.params = {};
+    return this;
+  }
+
+  Query.prototype.resolvedPath = function(){
+    var params = this.params || {}, path = this.path, ids = this.ids;
+    var ret = path.replace(/:(\w)+/ig, function(_,key){ 
+                return ids[key] || ':' + key; 
+              });
+    var qvars = [];
+    for (var k in params){
+      qvars.push( [esc(k),esc(params[k])].join('=') );
+    }
+    ret = ret + (qvars.length ? '?' + qvars.join('&') : '');
+    return ret;
+  }
+
+  Query.prototype.param = function(){
+    if (arguments.length == 0) return this;
+    switch(arguments.length){
+      case 1:
+        this.params = arguments[0]; break;
+      default:
+        this.params[arguments[0]] = arguments[1]; break;
+    }
+    return this;
+  }
+  
+  Query.prototype.run = function(fn) {
+    var model = this.model
+      , path = this.resolvedPath()
+      , url  = (path[0] == '/' ? path : model.url(this.resolvedPath()));
     request.get(url, function(res){
       if (res.error) return fn(error(res));
       var col = new Collection;
       for (var i = 0, len = res.body.length; i < len; ++i) {
-        col.push(new self(res.body[i]));
+        col.push(new model(res.body[i]));
       }
       fn(null, col);
     });
+    return this;
   }
-}
 
-function resolvePath(path,query,params){
-  query = query || {}; params = params || {};
-  var ret = path.replace(/:(\w)+/ig, function(_,key){ 
-              return params[key] || ':' + key; 
-            });
-  qvars = [];
-  for (var k in query){
-    if (params[k]) 
-      qvars.push( [esc(query[k]),esc(params[k])].join('=') );
-  }
-  ret = ret + (qvars.length ? '?' + qvars.join('&') : '');
-  return ret;
 }
 
